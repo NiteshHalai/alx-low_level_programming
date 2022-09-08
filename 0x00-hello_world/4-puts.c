612ad9309a5f6dedc253a122fefc8d791e60f645
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-08 12:36:09 -0700

    come on betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
+/* Prints with Put function */
+
 int main(void) {
-    
-  puts(" \"Programming is like building a multilingual puzzle");
-  
-  return 0;
-  
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
