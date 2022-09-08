574fe1537b9d2f366432bf56c768cb51568670fe
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-08 13:38:39 -0700

    Noww???

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
 *main - Entry point
 *Description: This programme uses puts to print out a string
 *Return: Always 0 (Success)
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-/*
+/**
 *main - Entry point
 *Description: 'the program's description'
 *Return: Always 0 (Success)
@@ -6,6 +6,6 @@
 #include <stdio.h>     
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, ");
+printf("with proper grammar, but the outcome is a piece of art,/n");
 return (0);  
 }
