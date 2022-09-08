04cdd4c3ca4646164c242229121f1f479a061d52
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-08 12:45:52 -0700

    Betty!!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,22 @@
 
 /* Prints with Put function */
 
-int main(void) {
+/**
+
+ * main - Entry point
+
+ * 
+
+ * Description: 'Prints a string'
+
+ * 
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
