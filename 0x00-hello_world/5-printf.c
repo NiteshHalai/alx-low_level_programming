f99ffcefeb596433251059d1d757b03de723049c
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-08 13:27:18 -0700

    Bettyyyyy!!!!

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-int main() {
-   
-  printf("with proper grammar, but the outcome is a piece of art,");
-  
-  return 0;
-  
+/*
+*main - Entry point
+*Description: 'the program's description'
+*Return: Always 0 (Success)
+*/
+
+#include <stdio.h>     
+
+int main(void) {
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);  
 }
