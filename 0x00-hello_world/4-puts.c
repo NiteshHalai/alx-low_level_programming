be60bf32294db17917bdddeeb29a9d595a4a44ea
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-08 11:28:40 -0700

    first c programe take 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  
-  // printf() displays the string inside quotation
-  
-  printf("Programming is like building a multilingual puzzle");
+    
+  puts("Programming is like building a multilingual puzzle");
   
   return 0;
   
