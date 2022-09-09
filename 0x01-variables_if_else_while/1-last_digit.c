2396020a69caad07b4c712097a90afcdb5d85c86
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-09 12:37:43 -0700

    last digit take 3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,25 +3,25 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
- *Description: 'checks whether number is positivr, nrgative, zero'
- *Return: 0
- */
+*main - Entry point
+*Description: 'checks the last digit'
+*Return: 0
+*/
 int main(void)
 {
-  int n;
-  int lastDigit;
+int n;
+int lastDigit;
   
-  srand(time(0));
+srand(time(0));
 
-  n = rand() - RAND_MAX / 2;
-  lastDigit = n % 10;
+n = rand() - RAND_MAX / 2;
+lastDigit = n % 10;
 
-  if  (lastDigit > 5)
-    printf("Last digit of %i is %i and is greater than 5\n", n, lastDigit);
-  else if (lastDigit == 0)
-    printf("Last digit of %i is %i and is 0\n", n, lastDigit);
-  else
-    printf("Last digit of %i is %i and is less than 6 and not 0", n, lastDigit);
-  return (0);
+if  (lastDigit > 5)
+printf("Last digit of %i is %i and is greater than 5\n", n, lastDigit);
+else if (lastDigit == 0)
+printf("Last digit of %i is %i and is 0\n", n, lastDigit);
+else
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastDigit);
+return (0);
 }
