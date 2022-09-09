c6133c129c332b1acba1b0f24d1f71bbd60d939d
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-09 13:41:28 -0700

    low case missing chars take 3

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,13 @@
 int main(void)
 {
 char ch = 'a';
-while (ch <= 'z')
-if (ch !='e' || ch !='q')
+while (ch <= 'z' && ch !='e' && ch !='q')
 {
 printf("%c", ch);
 ch++;
 }
+
+
 printf("\n");
 
 return (0);
