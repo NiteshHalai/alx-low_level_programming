3477e3d87af2e2efae3a24751edfb3aac747438d
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-09 12:44:24 -0700

    lowercase alphabet take 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+/**
+ *main - Entry point
+ *Description: 'prints alphabet in lowcase'
+ *Return: 0
+ */
+int main(void)
+{
+char ch = 'a';
+while(ch <= 'z')
+{
+printf("%c ", ch);
+ch++;
+}
+printf("\n");
+
+return (0);
+}
