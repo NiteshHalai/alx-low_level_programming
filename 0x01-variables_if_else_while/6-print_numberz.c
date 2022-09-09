b4db4fa01ed290bf5e2ded6f93d189eb2b6d0259
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-09 14:02:48 -0700

    print nums take 4

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description: 'prints numbers 1 to 9'
+*Return: 0
+*/
+int main(void)
+{
+int n = 0;
+while (n <= 9)
+{
+printf("%i", n);
+n++;
+}
+printf("\n");
+return (0);
+}
