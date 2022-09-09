64bda85ec0670151b7b65c83f55354063ad99c67
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-09 14:08:08 -0700

    print nums take 5

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description: 'prints alphabet in reverse lowcase'
+*Return: 0
+*/
+int main(void)
+{
+char ch = 'z';
+while (ch > 'A')
+{
+printf("%c", ch);
+ch--;
+}
+printf("\n");
+
+return (0);
+}
