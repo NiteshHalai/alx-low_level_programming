5b991066a63b726f19cf23a6517bb0f230f0168c
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-09 14:15:58 -0700

    print nums take 7

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description: 'prints numbers 1 to 15 in lowcase'
+*Return: 0
+*/
+int main(void)
+{
+int n = 0;
+while (n <= 15)
+{
+printf("%i", n);
+n++;
+}
+printf("\n");
+
+return (0);
+}
