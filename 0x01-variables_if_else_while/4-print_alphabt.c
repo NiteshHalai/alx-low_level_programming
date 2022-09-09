a0c589c86454d8b1b616ad733b326787ed74ced6
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-09 13:35:02 -0700

    low case missing chars take 1

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description: 'prints alphabet in lowcase'
+*Return: 0
+*/
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+if (ch !='e' or ch !='q')
+{
+printf("%c", ch);
+ch++;
+}
+printf("\n");
+
+return (0);
+}
