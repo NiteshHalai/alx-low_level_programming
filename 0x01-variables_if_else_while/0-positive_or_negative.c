3df10618362ae0f2387c004894934cb7cfce0ce6
Nitesh Halai <nitesh.halai@gmail.com>
2022-09-09 12:06:24 -0700

    fixed int formatting

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if  (n > 0)
-printf("%n is positive");
+printf("%i is positive", n);
 else if (n < 0)
-printf("%n is negative");
+printf("%i is negative", n);
 else
-printf("%n is zero", n);
+printf("%i is zero", n);
 return (0);
 }
