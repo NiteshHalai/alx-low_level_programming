d7e09df92ea38f8d0a825e66081273c7d73279a8
NiteshHalai <nitesh.halai@gmail.com>
2022-09-16 00:58:44 +0300

    Update 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+* _islower - Entry point
+*
+* Description: checls is character is lower case
+*
+*/
+int _islower(int c)
+{
+if  (c => 'z')
+return (1);
+else
+return (0);
+}
