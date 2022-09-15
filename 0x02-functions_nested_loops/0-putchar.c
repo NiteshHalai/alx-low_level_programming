3a00982d8999d5fa31c58a934d9f4ae67d398996
NiteshHalai <nitesh.halai@gmail.com>
2022-09-15 22:19:59 +0300

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+* main - Entry point
+*
+* Description: prints _putchar
+*
+* Return: 0
+*/
+
+int main(void)
+{
+char to_print = _putchar
+int i
+for (i = 0; i < len(_putchar); ++i)
+{
+  puchar(to_print[i])
+}
+putchar('\n)
+return (0)
+}
