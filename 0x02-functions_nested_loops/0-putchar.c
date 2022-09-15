767d0edc825eb432845a58459fb74cd619acb195
NiteshHalai <nitesh.halai@gmail.com>
2022-09-15 22:28:45 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-const char *to_print = "_putchar"
-int i
+const char *to_print = "_putchar";
+int i;
 for (i = 0; i < len(_putchar); ++i)
 {
   puchar(to_print[i])
 }
-putchar('\n')
+putchar('\n');
 return (0)
 }
