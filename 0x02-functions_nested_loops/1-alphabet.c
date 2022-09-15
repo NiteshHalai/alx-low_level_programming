4b6415e4f05e6db9b9239c17bd965c3db243e4be
NiteshHalai <nitesh.halai@gmail.com>
2022-09-15 22:57:51 +0300

    Update 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+* main - Entry point
+*
+* Description: prints alphabet in low case
+*
+* Return: 0
+*/
+int main(void)
+{
+
+char c;
+for (c = 'a'; c =< 'z'; ++c)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
