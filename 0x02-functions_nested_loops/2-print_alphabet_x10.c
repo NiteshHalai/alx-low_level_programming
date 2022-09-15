8defdf0ed210250c66d13ca99f21a897eeef1cbb
NiteshHalai <nitesh.halai@gmail.com>
2022-09-16 00:35:55 +0300

    Update 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+* print_alphabet_x10 - Entry point
+*
+* Description: prints alphabet in low case x10
+*
+*/
+void print_alphabet_x10(void)
+{
+
+int i
+char c;
+for (i = 1, ; i <= 10; ++1)
+{
+for (c = 'a'; c <= 'z'; ++c)
+{
+putchar(c);
+}
+)
+putchar('\n');
+}
