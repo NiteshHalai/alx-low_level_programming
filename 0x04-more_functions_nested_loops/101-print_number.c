09478f0b8e995d0bc7ea703f40938ef700a245ad
NiteshHalai <nitesh.halai@gmail.com>
2022-09-17 01:19:28 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,4 @@
-/* C program to print a long int number
- using putchar() only*/
-#include <stdio.h>
+#include"main.h"
  
 void print_number(int n)
 {
@@ -12,8 +10,8 @@ void print_number(int n)
  
 
     if (n/10)
-        _putchar(n/10);
+        print(n/10);
  
 
-    putchar(n%10 + '0');
+    _putchar(n%10 + '0');
 }
