ed58dba0178f9f905a3b9b487f542fba82903a00
NiteshHalai <nitesh.halai@gmail.com>
2022-09-17 00:52:59 +0300

    Update 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+* _isupper - function to check if
+* character is lowercase
+*
+* @c: checks input of function
+*
+* Return: returns output
+*/
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+return (0);
+}
