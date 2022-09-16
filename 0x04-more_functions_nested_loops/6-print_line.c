77d315c9842cbbc30ed2897ae23eb8a7061b23c4
NiteshHalai <nitesh.halai@gmail.com>
2022-09-17 01:48:40 +0300

    Update 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+*print_line - Entry point
+@c: checks input of function
+*Description: 'prints line'
+*/
+void print_line(int n)
+{
+int a;
+int q = '_';
+{
+for (a = 0; a <= n; ++a)
+putchar(q);
+}
+putchar('\n');
+}
