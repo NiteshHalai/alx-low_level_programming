88bce7c95c643989c314fa6a064cb0366cf34001
NiteshHalai <nitesh.halai@gmail.com>
2022-09-17 00:38:47 +0300

    Update 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+* _isdigit - function to check if
+* character is lowercase
+*
+* @c: checks input of function
+*
+* Return: returns output
+*/
+
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+return (1);
+return (0);
+}
