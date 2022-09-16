71ed886c732d6a239f245de68a955e8328723ae6
NiteshHalai <nitesh.halai@gmail.com>
2022-09-17 01:29:50 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,17 +1,21 @@
 #include"main.h"
- 
+
+/**
+* print_number - function to check if
+* character is lowercase
+*
+* @n: checks input of function
+*
+* Return: print number
+*/
+
 void print_number(int n)
 {
-
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
- 
-
-    if (n/10)
-        print_number(n/10);
- 
-
-    _putchar(n%10 + '0');
+if (n < 0) {
+_putchar('-');
+n = -n;
+} 
+if (n/10)
+print_number(n/10);
+_putchar(n%10 + '0');
 }
