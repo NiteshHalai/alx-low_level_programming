f3c0610022b66ad21d052522e43abe1ef694dcd5
NiteshHalai <nitesh.halai@gmail.com>
2022-09-20 00:00:21 +0300

    Update 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+* _strlen - swaps variable values
+*
+* @s: input function argument
+*
+* Return: outpuy
+*/
+
+int _strlen(char *s)
+{
+      int i = 0, sum = 0;
+      char c = s[0];
+
+      while(c != '\0') {
+            sum++;
+            c = s[++i];
+      }
+      return (sum);
+}
