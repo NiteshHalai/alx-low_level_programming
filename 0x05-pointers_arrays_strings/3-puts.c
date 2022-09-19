4e277137c76212e6df9e4d0f20aaeb13d7db45e5
NiteshHalai <nitesh.halai@gmail.com>
2022-09-20 00:51:07 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,18 +7,20 @@
 * Return: outpuy
 */
 void _puts(char *str)
+
 {
-    int i;
-    for(i=0;    ;i++)
+   int i = 0;
+   while(string[i])  
     {
-        if(*str[i]=='\0')
-        {
-            printf("\n");
-            break;
+        if( putchar(str[i]) == EOF)
+        { 
+            return EOF;
         }
-        printf("%c",*str);
-
+        i++;
     }
-
-}
+   if(putchar('\n') == EOF)
+   {
+       return EOF;
+   }
+   return 1; 
 }
