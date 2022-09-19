5cd2c7c0977774e0e83b9b2f52d2f3c9ed4ee47b
NiteshHalai <nitesh.halai@gmail.com>
2022-09-20 01:46:16 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,7 +7,22 @@
 * Return: outpuy
 */
 void print_rev(char *s)
-{  
-char s[40];
-strrev(s));  
-}  
+{
+int l, i;
+char *begin_ptr, *end_ptr, ch;
+l = strlen(s);
+begin_ptr = s;
+end_ptr = s;
+
+for (i = 0; i < l - 1; i++)
+end_ptr++;
+
+for (i = 0; i < l / 2; i++) {
+ch = *end_ptr;
+*end_ptr = *begin_ptr;
+*begin_ptr = ch;
+
+begin_ptr++;
+end_ptr--;
+}
+}
