45936ec87c352ffb374779b76193be8326ab9638
NiteshHalai <nitesh.halai@gmail.com>
2022-09-19 23:22:05 +0300

    Update 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+* swap_int - swaps variable values
+*
+* @n: input function argument
+*
+* Return: outpuy
+*/
+
+void swap_int(int *a, int *b)
+{
+*a = temp
+*a = *b
+*b = temp
+}
