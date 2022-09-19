8aff6c5326977d4bdb9e93d937dcb53e85ffab15
NiteshHalai <nitesh.halai@gmail.com>
2022-09-20 00:22:42 +0300

    Update 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+* _puts - prints string
+*
+* @str: input function argument
+*
+* Return: outpuy
+*/
+void _puts(char *str);
+{
+const char *to_print = *str";
+unsigned long int i;
+for (i = 0; i < strlen(to_print); ++i)
+{
+putchar(to_print[i]);
+}
+putchar('\n');
+}
