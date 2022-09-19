279881aacc13c9f4bf0788331001b379052a35bc
NiteshHalai <nitesh.halai@gmail.com>
2022-09-19 22:09:40 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include <ctype.h>
 
 /**
-* int _atoi - convert string to int
+* _atoi - convert string to int
 *
 * @s: input string
 *
@@ -11,9 +11,10 @@
 int _atoi(char *s)
 {
 int i, sum = 0;
-for (i = 0; isdigit(*s); s++) {
+for (i = 0; isdigit(*s); s++) 
+{
 sum = (sum * 10) + (*s - '0');
 i++;
 }
-return sum;
+return (sum);
 }
