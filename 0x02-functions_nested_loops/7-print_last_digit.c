8e5f73896a104fec5de600021a1d5397dfa58089
NiteshHalai <nitesh.halai@gmail.com>
2022-09-20 22:48:49 +0300

    Update 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+* print_last_digit - print last digit
+*
+* @c: checks input of function
+*
+* Return: returns 1 if `c` is lowercase
+*         otherwise 0
+*/
+
+int print_last_digit(int)
+{
+int n;
+n = int%10;
+return(n)
+}
