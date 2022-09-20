11cfabb548d3165dcb9614aefc9dab528b4781ac
NiteshHalai <nitesh.halai@gmail.com>
2022-09-20 19:44:37 +0300

    Update 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+* print_rev - reverse string
+*
+* @s: input function argument
+*
+* Return: outpuy
+*/
+
+void puts2(char *str)
+{
+for(int i = 0; i < strlen(*str); i+=2)
+{
+    putchar(input[i]);
+}
+}
