792a8d6bcdc4e26908198ce4b88d20986da82472
NiteshHalai <nitesh.halai@gmail.com>
2022-09-20 22:32:00 +0300

    Update 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+* print_sign - check sign
+*
+* @n: checks input of function
+*
+* Return: returns 0 if 0, -1 if less than zero
+*         1 is more than zero
+*/
+
+int print_sign(int n)
+{
+if (n == 0)
+return (0);
+else if (n < 0)
+return (-1);
+retutn (1)
+}
