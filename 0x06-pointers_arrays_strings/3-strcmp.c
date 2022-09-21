ee38cc358b1b41ed5876cab1c418ac01230e63fb
NiteshHalai <nitesh.halai@gmail.com>
2022-09-21 21:57:23 +0300

    Update 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+* _strncat - function that takes a pointer to an
+* int as parameter and updates the value it points to to 98
+*
+* @dest: input function argument
+* @src: input function argument
+* @n: input function argument
+*
+* Return: outpuy
+*/
+
+char *_strncat(char *dest, char *src, int n);
+{
+    char* ptr = dest + strlen(dest);
+ 
+    while (*src != '\0' && num--) {
+        *ptr++ = *src++;
+    }
+ 
+    *ptr = '\0';
+ 
+    return dest;
+}
