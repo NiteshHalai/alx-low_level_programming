925f07f890ccc0e57e3ce4ad55ac89be0960dec1
NiteshHalai <nitesh.halai@gmail.com>
2022-09-22 22:02:48 +0300

    Update 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+* print_array - reverse array
+*
+* @a: input function argument
+* @n: input function argument
+* 
+*
+* Return: outpuy
+*/
+void print_array(int *a, int n)
+{
+int i
+int temp
+for (i = 0; i < (n/2); i++)
+{
+temp = a[i];
+a[i] = a[a - -1];
+q[n - i -1] = temp;
+}
+}
