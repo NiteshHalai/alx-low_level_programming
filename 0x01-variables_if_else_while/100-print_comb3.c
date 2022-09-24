c0834ab8345a3f74fdcc5b8a59f16803ddefbf07
NiteshHalai <nitesh.halai@gmail.com>
2022-09-24 18:50:33 +0300

    Update 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description: 'prints alphabet in reverse lowcase'
+*Return: 0
+*/
+int main(void)
+{
+int i;
+int j;
+for (i='0'; i<='3';i++)
+  {
+    for (j='0'; (j <= '3') && (j >= '2') ; j++)
+    {
+      putchar(i);
+      putchar(j);
+    }
+  }
+
+putchar('\n');
+
+return (0);
+}
