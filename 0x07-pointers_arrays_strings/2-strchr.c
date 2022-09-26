5ee08b59933010357c85c3fce399179080e1cac9
NiteshHalai <nitesh.halai@gmail.com>
2022-09-26 22:16:38 +0300

    Update 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+* _strcat -  locates a character in a string
+*
+* @s: input function argument
+* @c: input function argument
+*
+* Return: output
+*/
+
+char *_strchr(char *s, char c)
+{
+while (*s != c) {
+if (! *s++) {
+printf("null\n");
+return NULL;
+            }
+}
+printf("%s", s);
+return (char *)s;
+}
