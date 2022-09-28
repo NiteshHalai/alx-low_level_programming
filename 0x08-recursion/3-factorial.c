a0cd6c582fabd34346b00ab00a27c6a94b4e2d80
NiteshHalai <nitesh.halai@gmail.com>
2022-09-29 00:17:26 +0300

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,7 +1,7 @@
 /**
 * factorial -  factorial of n
 *
-* n: input function argument
+* @n: input function argument
 *
 * Return: output
 */
@@ -12,5 +12,5 @@ if (n == 0)
 {
 return (1);
 }
-return ( n * factorial (n - 1));
+return (n * factorial(n - 1));
 }
