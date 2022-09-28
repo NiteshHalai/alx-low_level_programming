8d30661574c77aedfd8ee3ded80f8d9522568aae
NiteshHalai <nitesh.halai@gmail.com>
2022-09-29 00:19:40 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,18 +11,19 @@
 
 int _pow_recursion(int x, int y)
 {
-    
-    int store;
-    
-    if(y==0){return 1;}
-    
 
-    store = _pow_recursion(x,y/2);
+int store;
     
-    if(y%2==0)
-    return (store*store);
-    else
-    {
-        return (store*store*x);
-    }
+if(y == 0)
+{
+return 1;
+}
+store = _pow_recursion(x,y/2);
+
+if(y%2==0)
+return (store * store);
+else
+{
+return (store * store * x);
+}
 }
