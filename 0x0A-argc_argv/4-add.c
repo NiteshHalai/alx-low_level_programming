00bc9d619b498390533cde3e7722d202b1a1d699
NiteshHalai <nitesh.halai@gmail.com>
2022-10-01 01:05:07 +0300

    Update 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* @argc: input function argument
+* @argv: input function argument
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int a,b,sum;
+	int i;
+
+	
+	sum=0;
+	for(i=1; i<argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	
+	printf("%d\n",sum);
+	
+	return 0;
+}
