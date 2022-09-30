176b1379a2db3080fd967afcdd203dc4d33f5261
NiteshHalai <nitesh.halai@gmail.com>
2022-10-01 00:23:57 +0300

    Update 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description: This proggrame prints number of arguments
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%i\n",argc);
+return (0);
+}
