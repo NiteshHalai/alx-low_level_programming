37aec3fe6b19d3242e053c0965aee09ca37f7c7e
NiteshHalai <nitesh.halai@gmail.com>
2022-10-01 00:55:48 +0300

    Update 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main( int argc, char *argv[] )
+{
+    long result = 1;
+
+    for ( int i = 1; i < argc; i++ )
+    {
+        long num;
+        char *p;
+
+
+        errno = 0;
+        num = strtol( argv[i], &p, 10 );
+
+
+        if ( p == argv[i] )
+        {
+            fprintf( stderr, "Conversion of argv[%d] failed!\n", i );
+            exit( EXIT_FAILURE );
+        }
+
+
+        if ( errno == ERANGE )
+        {
+            fprintf( stderr, "Argument of argv[%d] out of range!\n", i );
+            exit( EXIT_FAILURE );
+        }
+
+
+        result = result * num;
+    }
+
+
+    printf( "%ld\n", result );
+
+    return EXIT_SUCCESS;
+}
