a7b3bf4552e5e2e497e6372ff409b98501b200a3
NiteshHalai <nitesh.halai@gmail.com>
2022-10-03 18:34:55 +0300

    Update 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include"main.h"
+
+/**
+* str_concat -  locates a character in a string
+*
+* @s1: input function argument
+* @s2: input function argument
+*
+* Return: output
+*/
+
+char *str_concat(char *s1, char *s2);
+{
+
+  int length, j;
+
+  length = 0;
+  while (s1[length] != '\0') {
+    ++length;
+  }
+
+  for (j = 0; s2[j] != '\0'; ++j, ++length)
+  {
+    s1[length] = s2[j];
+  }
+
+  s1[length] = '\0';
+
+  puts(s1);
+
+  return (s1);
+}
