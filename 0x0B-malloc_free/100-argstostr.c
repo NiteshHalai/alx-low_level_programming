5c531afaf541c7bcefdd2cba01d617237b590354
NiteshHalai <nitesh.halai@gmail.com>
2022-10-03 18:55:33 +0300

    Update 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,32 @@
+#include"main.h"
+
+/**
+* argstostr -  locates a character in a string
+*
+* @ac: input function argument
+* @av: input function argument
+*
+* Return: output
+*/
+
+char *argstostr(int ac, char **av)
+{
+    for(i=1; i<ac; i++) {
+        len += strlen(av[i]);
+    }
+
+    _all_args = all_args = (char *)malloc(len+ac-1);
+
+    for(i=1; i<ac; i++) {
+        memcpy(_all_args, av[i], strlen(av[i]));
+        _all_args += strlen(av[i])+1;
+        *(_all_args-1) = ' ';
+    }
+    *(_all_args-1) = 0;
+
+    printf("All %d args: '%s'\n", argc, all_args);
+
+    free(all_args);
+
+    return 0;
+}
