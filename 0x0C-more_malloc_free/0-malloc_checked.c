d4469727bf173f583e098b99585c4ba11d443d6f
NiteshHalai <nitesh.halai@gmail.com>
2022-10-06 20:45:59 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,1 @@
-#include"main.h"
 
-/**
-* malloc_checked -  what fucntion does
-*
-* @b: input function argument
-*
-* Return: output
-*/
-
-void *malloc_checked(unsigned int b)
-{
-void* ptr;
-ptr = (void*) malloc(sizeof(b));
-return (ptr);
-}
