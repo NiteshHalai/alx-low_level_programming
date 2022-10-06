ebac276989a7c49be28f43ce8083b0b71874c462
Nitesh Halai <nitesh.halai@gmail.com>
2022-10-06 10:51:55 -0700

    add

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -16,21 +16,3 @@ void *malloc_checked(unsigned int b)
   return (ptr);
   
 }
-=======
-#include"main.h"
-
-/**
-* malloc_checked -  what fucntion does
-*
-* @b: input function argument
-*
-* Return: output
-*/
-
-void *malloc_checked(unsigned int b)
-{
-void* ptr;
-ptr = (void*) malloc(sizeof(b));
-return (ptr);
-}
->>>>>>> 843bbb4f2aa03740cd68fcb8bd36340b324203ad
