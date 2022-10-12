1ba8c569902b2caf4fbc727ece3ae5bd87bff61d
NiteshHalai <nitesh.halai@gmail.com>
2022-10-12 22:23:24 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,7 @@ float age;
 char *owner;
 };
 
-#endif /*_DOG_H */
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -26,3 +26,4 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
+#endif /*_DOG_H */
