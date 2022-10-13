020c3f61446207e4baa807c2fe1f0052c24b7715
NiteshHalai <nitesh.halai@gmail.com>
2022-10-13 21:34:32 +0300

    Update 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* init_dog -  description
+*
+* @d: input function argument
+* @name: input function argument
+* @age: input function argument
+* @owner: input function argument
+*
+* Return: output
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+
+
+}
