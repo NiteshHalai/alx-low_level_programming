000c5c9269c6edf37b821dde7dc388bf7da949af
NiteshHalai <nitesh.halai@gmail.com>
2022-10-13 22:18:25 +0300

    Update 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator -  description
+*
+* @array: input function argument
+* @size: input function argument
+* @action: input function argument
+*
+* Return: output
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+action(array);
+
+}
