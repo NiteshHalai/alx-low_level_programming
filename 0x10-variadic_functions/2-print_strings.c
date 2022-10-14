05ab69731514b76e8a607aa4e83f22578fff0881
NiteshHalai <nitesh.halai@gmail.com>
2022-10-14 21:43:49 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,6 +11,18 @@
 * Return: output
 */
 
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_numbers -  description
+*
+* @separator: input function argument
+* @n: input function argument
+*
+* Return: output
+*/
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 const char *sep;
@@ -21,12 +33,24 @@ va_list args;
 va_start(args, n);
 
 sep = "";
+if (separator == NULL)
+{
+for (i = 0; i < n; i++)
+{
+const char *value = va_arg(args, const char*);
+printf("%s%s", sep, value);
+}
+}
+else
+{
 for (i = 0; i < n; i++)
 {
 const char *value = va_arg(args, const char*);
 printf("%s%s", sep, value);
 sep = separator;
 }
+}
 printf("\n");
 
 }
+
