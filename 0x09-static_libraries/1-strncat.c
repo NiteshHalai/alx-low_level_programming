5dcaf5d5a93ad141648aac24f176baa3091f0083
NiteshHalai <nitesh.halai@gmail.com>
2022-10-20 21:36:43 +0300

    Update 1-strncat.c

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+* _strcat -  description
+*
+* @dest: input function argument
+* @src: input function argument
+*
+* Return: output
+*/
+
+char *_strcat(char *dest, char *src)
+{
+return (0);
+}
