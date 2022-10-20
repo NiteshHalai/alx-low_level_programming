3765dab5249ee9006c7487a76d98cdbccb1b21b9
NiteshHalai <nitesh.halai@gmail.com>
2022-10-20 20:25:08 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,13 +11,19 @@
 */
 
 unsigned int _strspn(char *s, char *accept)
-int i,k,counter=0;
-for(i=0;s[i]!='\0';i++){
+int i;
+int k;
+int counter;
+
+counter = 0;
+
+for(i=0; s[i]!='\0'; i++)
+{
  if(counter != i) break;
  for(k=0;accept[k]!='\0';k++){
   if(s[i]==accept[k])
    counter++;
  }
 }
- return counter;
+ return (counter);
 }
