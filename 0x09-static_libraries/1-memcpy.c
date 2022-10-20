2fefdd12c5f38d6ada4c8cfc2a61ba7d8e521890
NiteshHalai <nitesh.halai@gmail.com>
2022-10-20 21:34:47 +0300

    Update 1-memcpy.c

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+* _memcpy -  description
+*
+* @dest: input function argument
+* @src: input function argument
+* @n: input function argument
+*
+* Return: output
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+return (0);
+}
