efa288fed6066809916afb010b7558b3cba00a60
NiteshHalai <nitesh.halai@gmail.com>
2022-10-20 20:42:03 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,18 +12,18 @@
 char *_strpbrk(char *s, char *accept)
 {
 int i, j, pos, flag = 0;
-for (i = 0; s[i] != '\0';i++);
+for (i = 0; s[i] != '\0'; i++);
 pos = i;
-for (i = 0;accept[i] != '\0';i++)
+for (i = 0; accept[i] != '\0';i++)
 {
-for (j = 0; s[j] != '\0';j++)
+for (j = 0; s[j] != '\0'; j++)
 {
 if (accept[i] == s[j])
 {
 if (j <= pos)
 {
-pos = j;    
-flag = 1;    
+pos = j;
+flag = 1;
 }
 }
 }        
