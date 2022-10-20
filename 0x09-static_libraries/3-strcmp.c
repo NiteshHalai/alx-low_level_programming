ceafcfc0482af892d7d9f441f8531aa44e85104d
NiteshHalai <nitesh.halai@gmail.com>
2022-10-20 21:45:04 +0300

    Update 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+* _strcmp -  description
+*
+* @s1: input function argument
+* @s2: input function argument
+*
+* Return: output
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+return (0);
+}
