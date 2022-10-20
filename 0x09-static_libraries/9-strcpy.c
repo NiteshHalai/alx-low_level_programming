61d0b5fb094a0002857025075f95ac853da9c4b5
NiteshHalai <nitesh.halai@gmail.com>
2022-10-20 21:52:07 +0300

    Update 9-strcpy.c

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+* _strcpy -  description
+*
+* @dest: input function argument
+* @src: input function argument
+*
+* Return: output
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+return (0);
+}
