db1f70900e9866afa2582820b31bd801aa048a15
NiteshHalai <nitesh.halai@gmail.com>
2022-10-25 00:23:23 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,12 +14,15 @@ list_t *add_node(list_t **head, const char *str)
 {
       
 list_t *newNode = malloc(sizeof(list_t));
-      newNode->str = strdup(str);
-      newNode->len = strlen(str);
+if (!newNode)
+return (NULL);
+      
+newNode->str = strdup(str);
+newNode->len = strlen(str);
 
-      newNode->next = *head;
+newNode->next = *head;
       
-      *head = newNode;
+*head = newNode;
       
-      return(*head);
+return(*head);
 }
