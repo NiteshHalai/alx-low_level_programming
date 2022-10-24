9c9a4ec0af96daf9be87f397206c0b87e01009cd
NiteshHalai <nitesh.halai@gmail.com>
2022-10-25 01:11:17 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -7,4 +7,4 @@ main:                                       ; This is called by the C library st
           call      puts                    ; puts(message)
           ret                               ; Return from main back into C library wrapper
 message:
-          db        "Hello, Holberton", 0        ; Note strings must be terminated with 0 in C
+          db        "Hello, Holberton\n", 0        ; Note strings must be terminated with 0 in C
