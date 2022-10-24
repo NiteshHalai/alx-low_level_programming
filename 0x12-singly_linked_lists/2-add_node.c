196f2c03d001c34b9e63257777e5b73e93b41586
NiteshHalai <nitesh.halai@gmail.com>
2022-10-25 00:04:43 +0300

    Update 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include"lists.h"
+
+/**
+* add_node -  description
+*
+* @head: input function argument
+* @str: input function argument
+*
+* Return: output
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+list_t *head = NULL;
+
+list_t *newNode = malloc(sizeof(list_t));
+      newNode->str = str;
+      newNode->len = strlen(str);
+
+}
