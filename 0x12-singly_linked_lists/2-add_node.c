a29d4fb5cd26e549be21b7e4f556b9e33f42c30d
NiteshHalai <nitesh.halai@gmail.com>
2022-10-25 00:20:46 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
       
 list_t *newNode = malloc(sizeof(list_t));
       newNode->str = strdup(str);
-      newNode->len = strlen(new_str);
+      newNode->len = strlen(str);
 
       newNode->next = *head;
       
