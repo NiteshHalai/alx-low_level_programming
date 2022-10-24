91c943c3936bc1f27461e4a87ad17c8862d5ff5a
NiteshHalai <nitesh.halai@gmail.com>
2022-10-25 01:01:54 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,8 @@
 * Return: output
 */
 
-void myStartupFun (void)
+void myStartupFun(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n")
+printf("\nI bore my house upon my back!\n");
 }
