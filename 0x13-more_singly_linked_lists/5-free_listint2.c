ddd77eaafc342143863fbdd95a0ff6184234afc9
NiteshHalai <nitesh.halai@gmail.com>
2022-10-27 22:36:05 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,11 +13,11 @@ void free_listint2(listint_t **head)
 {
 listint_t *pNode = *head, *pNext;
 
-while (pNode != NULL)
+while (*pNode != NULL)
 {
-pNext = pNode->next;
-free(pNode);
-pNode = pNext;
+pNext = (*pNode)->next;
+free(*pNode);
+*pNode = pNext;
 }
 
 }
