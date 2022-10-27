b0eafbe4bac3ea661c1b7ecc80d82dc8e39e7b90
NiteshHalai <nitesh.halai@gmail.com>
2022-10-27 22:18:33 +0300

    Update 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include"lists.h"
+#include <string.h>
+
+/**
+* free_listint2 -  description
+*
+* @head: input function argument
+*
+* Return: output
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *pNode = head, *pNext;
+
+while (pNode != NULL)
+{
+pNext = pNode->next;
+free(pNode);
+pNode = pNext;
+}
+
+}
