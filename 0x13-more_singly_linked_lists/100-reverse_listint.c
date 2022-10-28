bfbcf2e6dfc5071f9313db92c2d7f59910d73e23
NiteshHalai <nitesh.halai@gmail.com>
2022-10-29 00:56:41 +0300

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,16 +1,27 @@
+#include"lists.h"
+#include <string.h>
+
+/**
+* reverse_listint -  description
+*
+* @head: input function argument
+*
+* Return: output
+*/
+
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t prev = NULL;
-    listint_t current = *head;
-    listint_t next = NULL;
-    while (current != NULL) {
-        next = current->next;
+listint_t *prev = NULL;
+listint_t *current = *head;
+listint_t *next = NULL;
+
+while (current != NULL) {
+next = current->next;
+current->next = prev;
  
-        current->next = prev;
- 
-        prev = current;
-        current = next;
-    }
-    *head = prev;
-    return(head)
+prev = current;
+current = next;
+}
+*head = prev;
+return(*head);
 }
