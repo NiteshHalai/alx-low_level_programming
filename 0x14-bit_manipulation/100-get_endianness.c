2612279b0839e51e8b6c75be3853eca324703c96
NiteshHalai <nitesh.halai@gmail.com>
2022-11-01 00:25:28 +0300

    Update 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+* get_endianness -  description
+*
+* Return: output
+*/
+
+int get_endianness(void)
+{
+unsigned int x = 0x76543210;
+char *c = (char*) &x;
+if (*c == 0x10)
+{
+return (1);
+}
+else
+return (0);
+}
