1f3a0aad0975a92fbe31d2cc65205c003a81239d
NiteshHalai <nitesh.halai@gmail.com>
2022-11-02 22:41:53 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,14 +23,13 @@ if (!buf)
 return (0);
 
 fd = open(filename, O_RDONLY);
- 
-if(fd == -1)
-{return(0);};
 
- 
+if(fd == -1)
+return(0);
+
 nrd = read(fd, buf, letters);
 nwr = write(STDOUT_FILENO, buf, nrd);
- 
+
 close(fd);
 return (nwr);
 }
