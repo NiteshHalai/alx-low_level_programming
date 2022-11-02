4efeb5304eaf79b91e934aa631bd2ce5c3277af7
NiteshHalai <nitesh.halai@gmail.com>
2022-11-02 22:37:51 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
-#include <stdio.h>
+#include"main.h"
 #define _POSIX_C_SOURCE  200809L
 #define _GNU_SOURCE
 
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(int ac, char **av)
-{
-    ssize_t n;
+* read_textfile -  description
+*
+* @filename: input function argument
+* @letters: input function argument
+*
+* Return: output
+*/
 
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
-    return (0);
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+    char *buf;
+    int fd;
+    ssize_t nrd, nwr;
+
+    buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+    fd = open(filename, O_RDONLY);
+ 
+   if(fd == -1)
+    {return(0);};
+
+ 
+    nrd = read(fd, buf, letters);
+    nwr = write(STDOUT_FILENO, buf, nrd);
+ 
+    close(fd);
+    return (nwr);
 }
